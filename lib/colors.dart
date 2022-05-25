43273ab5db0b5784f10857e8ea974fddaae216b8
nguyen duy sim <nguyen.duy.sim@sun-asterisk.com>
2022-05-25 18:25:26 +0700

    emplement login screen

---
+++ lib/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class ColorsApp {
+  static const crusta = const Color(0xFFFF7B43);
+  static const violetRed = const Color(0xFFf4326f);
+  static const bittersweet = const Color(0xFFff6666);
+  
+}
